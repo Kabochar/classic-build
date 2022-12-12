8fe8c8a3585a59019967f8f20677052351c36a25
Kabochar <anemailbox@163.com>
2022-12-12 20:11:28 +0800

    feat: add pong service

--- web_framework/gin/v1/cmd/server/main.go
+++ web_framework/gin/v1/cmd/server/main.go
@@ -1,15 +1,10 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin/v1/server"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"code":    0,
-			"message": "pong",
-			"demo":    "中文字符串处理",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	engine := server.NewRouter()
+	engine.Run() // listen and serve on 0.0.0.0:8080
 }
