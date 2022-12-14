71193ea7f768f46f1edfa62c5482d9880577c9a1
kabocha <anemailbox@163.com>
2022-12-15 01:47:41 +0800

    perf(gorm): config slowthreshold param to show all query sql

--- web_framework/gin/v1/models/init.go
+++ web_framework/gin/v1/models/init.go
@@ -18,10 +18,10 @@ func NewDatabase(connString string) {
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
-			SlowThreshold:             time.Second,   // Slow SQL threshold
-			LogLevel:                  logger.Silent, // Log level
-			IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
-			Colorful:                  false,         // Disable color
+			SlowThreshold:             1 * time.Microsecond, // Slow SQL threshold
+			LogLevel:                  logger.Info,          // Log level
+			IgnoreRecordNotFoundError: true,                 // Ignore ErrRecordNotFound error for logger
+			Colorful:                  false,                // Disable color
 		},
 	)
 	db, err := gorm.Open(sqlite.Open(".demo.db"), &gorm.Config{
