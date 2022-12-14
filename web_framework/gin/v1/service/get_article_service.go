b5f3af3f84413afbf14463f6fc6e7c8ce1fb57a6
kabocha <anemailbox@163.com>
2022-12-15 01:48:36 +0800

    feat(article): impl list, get, update, delete func

--- web_framework/gin/v1/service/get_article_service.go
+++ web_framework/gin/v1/service/get_article_service.go
@@ -1,1 +1,22 @@
 package service
+
+import (
+	"context"
+	"log"
+
+	"gin/v1/models"
+)
+
+type GetArticleService struct {
+}
+
+func (svc *GetArticleService) GetArticle(ctx context.Context, id int64) interface{} {
+	var article models.Article
+
+	if err := models.DB.WithContext(ctx).First(&article, id).Error; err != nil {
+		log.Println("find record err", err)
+		return err
+	}
+
+	return article
+}
--- web_framework/gin/v1/service/update_article_service.go
+++ web_framework/gin/v1/service/update_article_service.go
@@ -1,8 +1,41 @@
 package service
 
+import (
+	"context"
+	"log"
+
+	"github.com/jinzhu/copier"
+
+	"gin/v1/models"
+	"gin/v1/pkg/typex"
+)
+
 type UpdateArticleService struct {
+	Title   string         `json:"title" form:"title" `
+	Alias   typex.StrSlice `json:"alias" form:"alias"`
+	Author  string         `json:"author" form:"author"`
+	Content string         `json:"content" form:"content"`
+	Tags    typex.StrSlice `json:"tags" form:"tags"`
 }
 
-func (UpdateArticleService) UpdateArticle() {
+func (svc *UpdateArticleService) UpdateArticle(ctx context.Context, id int64) interface{} {
+	var article models.Article
 
+	tx := models.DB.WithContext(ctx)
+	if err := tx.First(&article, id).Error; err != nil {
+		log.Println("find record err ", err)
+		return err
+	}
+
+	copier.Copy(&article, svc)
+	if err := tx.Updates(&article).Error; err != nil {
+		log.Println("update record err ", err)
+		return err
+	}
+
+	if err := tx.First(&article, id).Error; err != nil {
+		log.Println("find record err ", err)
+		return err
+	}
+	return &article
 }
